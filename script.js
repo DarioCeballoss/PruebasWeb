39240cff0d31245f1be93c48c1a0cd1caf68da89
Glitch (stripe-noon-chamomile) <none>
2022-05-17 14:08:37 +0000

    CIRCULO DE PROGRESO  2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-window.onload = function circuloDeProgreso() {
-  var can = document.getElementById('canvas'),
-      spanProcent = document.getElementById('procent'),
+function ayer() {
+  var can = document.getElementById('canvas2'),
+      spanProcent = document.getElementById('procent2'),
        c = can.getContext('2d');
  
   // POSICION DEL CIRCULO
-  var posX = can.width / 2, 
+  let posX = can.width / 2, 
       posY = can.height / 2,
       fps = 1000 / 200, //velocidad de progreso
       procent = 50,     // valor del porcentaje en el medio TEXTO
@@ -31,7 +31,7 @@ window.onload = function circuloDeProgreso() {
       c.stroke();
 
       c.beginPath();
-      c.strokeStyle = '#3949AB'; // Color del circulo de porcentaje
+      c.strokeStyle = 'red'; // Color del circulo de porcentaje
       c.lineWidth = '10';// espesor del principal
       c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + deegres) );
       c.stroke();
@@ -40,5 +40,38 @@ window.onload = function circuloDeProgreso() {
     
   }
   
+}
+
+//Creando codigo que yo entienda
+
+function progresCircule(){
+    const can = document.getElementById("canvas1234"),
+        nProcent = document.getElementById("procent1234"),
+        c = can.getContext('2d');
+
+  // POSICION DEL CIRCULO
+  let posX = can.width / 2, 
+      posY = can.height / 2,
+      fps = 1000 / 200, //velocidad de progreso
+      procent = 30,     // valor del porcentaje en el medio TEXTO
+      oneProcent = 360 / 100, //transforma el perimetro en porcentaje
+      result = oneProcent * procent;// porcentaje de progreso 
+
+
+ nProcent.innerHTML = procent;
+      
+      c.clearRect( 0, 0, can.width, can.height );
+
+      c.beginPath();
+      c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + 360) );  // Circulo de abajo
+      c.strokeStyle = '#b1b1b3'; // color del anterior
+      c.lineWidth = '10';// espesor del anterior
+      c.stroke();
+
+      c.beginPath();
+      c.strokeStyle = 'red'; // Color del circulo de porcentaje
+      c.lineWidth = '10';// espesor del principal
+      c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + result) );
+      c.stroke();
   
 }
